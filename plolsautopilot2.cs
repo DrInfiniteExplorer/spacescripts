4cfeeeb5728209588896b0143ea78aa296856676
Pelle Månsson <pelle.mansson@gmail.com>
2021-01-31 17:25:05 +0100

    Update plolsautopilot2.cs

--- plolsautopilot2.cs
+++ plolsautopilot2.cs
@@ -25,21 +25,191 @@ namespace SpaceEngineers.UWBlockPrograms.BatteryMonitor {
 
 private MyIni storageIni = new MyIni();
 private MyIni configIni = new MyIni();
-private IMyTextSurface textOutputSurface;
 private IMyShipController controller;
-
-private string permanentlySavedInitString = null;
-private StringBuilder textOutputStringBuilder = new StringBuilder();
-
 private string state;
-private int currentWaypointIndex;
+private List<Waypoint> waypoints;
+private ThrustController thrustController;
+private FrameText frameText;
+private WaypointFollower waypointFollower;
+long frameCounter = 0;
 
+public Program() {
+    Runtime.UpdateFrequency = UpdateFrequency.Update10;
+    var storageParsedOk = storageIni.TryParse(Storage);
+    state = storageIni.Get("state", "state").ToString("init");
 
+    var configParsedOk = configIni.TryParse(Me.CustomData);
+    var textOutputBlockName = configIni.Get("blocks", "textOutput").ToString().Split(':');
+    var textOutputBlock = GridTerminalSystem.GetBlockWithName(textOutputBlockName[0]);
+
+    frameText = new FrameText(textOutputBlock as IMyTextSurface ?? (textOutputBlock as IMyTextSurfaceProvider)
+        ?.GetSurface(textOutputBlockName.Length > 1 ? int.Parse(textOutputBlockName[1]) : 0));
+    frameText.PreludeWriteln($"storage {storageParsedOk} config {configParsedOk}");
+
+    var controllerBlock = GridTerminalSystem.GetBlockWithName(configIni.Get("blocks", "controller").ToString());
+    controller = controllerBlock as IMyShipController;
+
+    var keys = new List<MyIniKey>();
+    configIni.GetKeys("waypoints", keys);
+    waypoints = keys.Select(key => Waypoint.FromString(key.Name, configIni.Get(key).ToString())).ToList();
+
+    var allThrustersOnShip = new List<IMyThrust>();
+    GridTerminalSystem.GetBlocksOfType<IMyThrust>(allThrustersOnShip);
+    thrustController = new ThrustController(allThrustersOnShip);
+}
+public void Save() {
+    storageIni.Set("state", "state", state);
+    Storage = storageIni.ToString();
+}
+public void Main(string args) {
+    frameText.StartFrame();
+    try {
+        if (args == "createWaypoint") {
+            var newWp = new Waypoint {
+                Name = $"autoCreated{waypoints.Count+1}",
+                WorldPos = Me.GetPosition(),
+                ApproachSpeed = 2,
+                Radius = 15
+            };
+            waypoints.Add(newWp);
+            configIni.Set("waypoints", newWp.Name, $"{newWp.WorldPos.X:0.00},{newWp.WorldPos.Y:0.00},{newWp.WorldPos.Z:0.00},{newWp.Radius},{newWp.ApproachSpeed},");
+            Me.CustomData = configIni.ToString();
+        } else {
+            Frame(args);
+        }
+    } catch (Exception e) {
+        frameText.Writeln($"Error: {e}");
+    }
+    frameText.EndFrame();
+}
+private void Frame(string args) {
+    var currentWorldPos = Me.GetPosition();
+    if (state == "init") {
+        if (waypoints.Count > 0) {
+            state = "followingWaypoints";
+            waypointFollower = new WaypointFollower(waypoints, controller, thrustController);
+        } else {
+            frameText.Writeln("No waypoints :(");
+        }
+    }
+    frameText.Writeln($"State = {state} ({frameCounter++})");
+    if (state == "waiting") {
+        if (args == "continue") {
+            state = "followingWaypoints";
+        }
+    }
+    if (state == "follwingWaypoints") {
+        if (args == "wait") {
+            thrustController.ResetThrusters();
+            state = "waiting";
+        }
+    }
+    if (state == "followingWaypoints") {
+        frameText.Writeln($"Heading to {waypointFollower.Current.Name}");
+        waypointFollower.Go(Me.GetPosition(), Me.WorldMatrix, (s, wp, actions) => {
+            foreach (var action in actions) {
+                if (action.Action == "wait") {
+                    state = "waiting";
+                    thrustController.ResetThrusters();
+                } else if (action.Action == "notify") {
+                    var block = (GridTerminalSystem.GetBlockWithName(action.ComputerName) as IMyProgrammableBlock);
+                    block?.TryRun(action.Argument);
+                }
+            }
+            if (s == "finished") {
+                waypointFollower = new WaypointFollower(waypoints, controller, thrustController);
+            }
+        });
+    }
+}
+private class WaypointFollower {
+    List<Waypoint> waypoints;
+    private IMyShipController controller;
+    ThrustController thrustController;
+    int currentWaypointIndex;
+    public Waypoint Current { get { return waypoints[currentWaypointIndex]; } }
+    public WaypointFollower(List<Waypoint> waypointsToFollow, IMyShipController shipController, ThrustController oof) {
+        waypoints = waypointsToFollow;
+        controller = shipController;
+        thrustController = oof;
+        currentWaypointIndex = 0;
+    }
+    public void Go(Vector3D currentWorldPos, MatrixD worldMatrix, Action<string, Waypoint, List<OnArriveAction>> f) {
+        if ((Current.WorldPos - currentWorldPos).Length() < Current.Radius) {
+            if (currentWaypointIndex + 1 >= waypoints.Count) {
+                f("finished", Current, Current.OnArriveActions);
+                return;
+            }
+            f("passedWaypoint", Current, Current.OnArriveActions);
+            currentWaypointIndex += 1;
+        }
+        var worldToShipCoords = Matrix.Transpose(worldMatrix);
+        var delta = Vector3D.TransformNormal(Current.WorldPos - currentWorldPos, worldToShipCoords);
+        var currentVelocity = Vector3D.TransformNormal(controller.GetShipVelocities().LinearVelocity, worldToShipCoords);
+        thrustController.ApproachVelocity(currentVelocity, Vector3D.Normalize(delta) * Current.ApproachSpeed);
+    }
+}
+private class ThrustController {
+    public Dictionary<Base6Directions.Direction, List<IMyThrust>> ThrustersByDirection { get; set; }
+    public ThrustController(List<IMyThrust> allowedThrusters) {
+        ThrustersByDirection = new Dictionary<Base6Directions.Direction, List<IMyThrust>>();
+        ThrustersByDirection[Base6Directions.Direction.Right] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(1, 0, 0)).ToList();
+        ThrustersByDirection[Base6Directions.Direction.Left] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(-1, 0, 0)).ToList();
+        ThrustersByDirection[Base6Directions.Direction.Up] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 1, 0)).ToList();
+        ThrustersByDirection[Base6Directions.Direction.Down] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(0, -1, 0)).ToList();
+        ThrustersByDirection[Base6Directions.Direction.Backward] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 0, 1)).ToList();
+        ThrustersByDirection[Base6Directions.Direction.Forward] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 0, -1)).ToList();
+    }
+    public void ResetThrusters() {
+        foreach (var p in ThrustersByDirection) {
+            foreach (var t in p.Value) {
+                t.ThrustOverridePercentage = 0;
+            }
+        }
+    }
+    public void ApproachVelocity(Vector3D currentVelocity, Vector3D desiredVelocity) {
+        var velocityDelta = desiredVelocity - currentVelocity;
+        ControlThrusters(velocityDelta.X, ThrustersByDirection[Base6Directions.Direction.Left], ThrustersByDirection[Base6Directions.Direction.Right], desiredVelocity.Length());
+        ControlThrusters(velocityDelta.Y, ThrustersByDirection[Base6Directions.Direction.Down], ThrustersByDirection[Base6Directions.Direction.Up], desiredVelocity.Length());
+        ControlThrusters(velocityDelta.Z, ThrustersByDirection[Base6Directions.Direction.Forward], ThrustersByDirection[Base6Directions.Direction.Backward], desiredVelocity.Length());
+    }
+    private void ControlThrusters(double velocityDeltaComponent, List<IMyThrust> positiveThrusters, List<IMyThrust> negativeThrusters, double approachSpeed) {
+        var thrustSetting = (float) (Math.Abs(velocityDeltaComponent) / approachSpeed);
+        foreach (var t in positiveThrusters) {
+            t.ThrustOverridePercentage = velocityDeltaComponent > 0 ? thrustSetting : 0;
+        }
+        foreach (var t in negativeThrusters) {
+            t.ThrustOverridePercentage = velocityDeltaComponent < 0 ? thrustSetting : 0;
+        }
+    }
+}
+private class FrameText {
+    private StringBuilder preludeStringBuilder;
+    private StringBuilder frameStringBuilder;
+    private IMyTextSurface textOutputSurface;
+    public FrameText(IMyTextSurface surface) {
+        textOutputSurface = surface;
+        preludeStringBuilder = new StringBuilder();
+        frameStringBuilder = new StringBuilder();
+    }
+    public void StartFrame() {
+        frameStringBuilder.Clear();
+    }
+    public void EndFrame() {
+        textOutputSurface?.WriteText(preludeStringBuilder.ToString());
+        textOutputSurface?.WriteText(frameStringBuilder.ToString(), true);
+    }
+    public void Writeln(string s) {
+        frameStringBuilder.Append(s + "\n");
+    }
+    public void PreludeWriteln(string s) {
+        preludeStringBuilder.Append(s + "\n");
+    }
+}
 private struct OnArriveAction {
     public string Action { get; set; }
     public string ComputerName { get; set; }
     public string Argument { get; set; }
-
     public static List<OnArriveAction> FromString(string s) {
         return s.Split('+')
             .Select(a => {
@@ -63,16 +233,13 @@ private struct OnArriveAction {
             .ToList();
     }
 }
-
 private struct Waypoint {
     public string Name { get; set; }
     public Vector3D WorldPos { get; set; }
     public double Radius { get; set; }
     public double ApproachSpeed { get; set; }
     public List<OnArriveAction> OnArriveActions { get; set; }
-
-    public static Waypoint FromConfigLine(string name, string configLine) {
-
+    public static Waypoint FromString(string name, string configLine) {
         var a = configLine.Split(',').ToList();
         return new Waypoint() {
             Name = name,
@@ -90,214 +257,6 @@ private struct Waypoint {
             return defaultValue;
         }
     }
-}
-
-private List<Waypoint> waypoints;
-
-Dictionary<Base6Directions.Direction, List<IMyThrust>> thrusters = new Dictionary<Base6Directions.Direction, List<IMyThrust>>();
-
-public Program() {
-    Runtime.UpdateFrequency = UpdateFrequency.Update10;
-    
-    var storageParsedOk = storageIni.TryParse(Storage);
-    state = storageIni.Get("state", "state").ToString("init");
-
-    var configParsedOk = configIni.TryParse(Me.CustomData);
-
-    ScreenWriteln($"storage {storageParsedOk} config {configParsedOk}");
-
-    var textOutputBlock = GridTerminalSystem.GetBlockWithName(configIni.Get("blocks", "textOutput").ToString());
-
-    textOutputSurface = textOutputBlock as IMyTextSurface ?? (textOutputBlock as IMyTextSurfaceProvider)?.GetSurface(0);
-    if (textOutputSurface != null) {
-        textOutputSurface.ContentType = VRage.Game.GUI.TextPanel.ContentType.TEXT_AND_IMAGE;
-        textOutputSurface.FontSize = 1;
-        textOutputSurface.Alignment = VRage.Game.GUI.TextPanel.TextAlignment.LEFT;
-    }
-
-    var controllerBlock = GridTerminalSystem.GetBlockWithName(configIni.Get("blocks", "controller").ToString());
-    controller = controllerBlock as IMyShipController;
-
-
-    var keys = new List<MyIniKey>();
-    configIni.GetKeys("waypoints", keys);
-    waypoints = keys.Select(key => Waypoint.FromConfigLine(key.Name, configIni.Get(key).ToString())).ToList();
-
-    var savedWaypointName = storageIni.Get("state", "currentWaypointName").ToString();
-    currentWaypointIndex = waypoints.FindIndex(w => w.Name == savedWaypointName);
-
-    if (currentWaypointIndex == -1) {
-        currentWaypointIndex = 0;
-        state = "init";
-    }
-
-    var allThrustersOnShip = new List<IMyThrust>();
-    GridTerminalSystem.GetBlocksOfType<IMyThrust>(allThrustersOnShip);
-    thrusters[Base6Directions.Direction.Right] = allThrustersOnShip.Where(t => t.GridThrustDirection == new Vector3I(1, 0, 0)).ToList();
-    thrusters[Base6Directions.Direction.Left] = allThrustersOnShip.Where(t => t.GridThrustDirection == new Vector3I(-1, 0, 0)).ToList();
-    thrusters[Base6Directions.Direction.Up] = allThrustersOnShip.Where(t => t.GridThrustDirection == new Vector3I(0, 1, 0)).ToList();
-    thrusters[Base6Directions.Direction.Down] = allThrustersOnShip.Where(t => t.GridThrustDirection == new Vector3I(0, -1, 0)).ToList();
-    thrusters[Base6Directions.Direction.Backward] = allThrustersOnShip.Where(t => t.GridThrustDirection == new Vector3I(0, 0, 1)).ToList();
-    thrusters[Base6Directions.Direction.Forward] = allThrustersOnShip.Where(t => t.GridThrustDirection == new Vector3I(0, 0, -1)).ToList();
-}
-
-public void Save() {
-    storageIni.Set("state", "state", state);
-
-    Storage = storageIni.ToString();
-}
-
-long frameCounter = 0;
-
-public void Main(string args) {
-    StartFrame();
-    try {
-        if (args == "createWaypoint") {
-            var newWp = new Waypoint {
-                Name = $"autoCreated{waypoints.Count+1}",
-                WorldPos = Me.GetPosition(),
-                ApproachSpeed = 2,
-                Radius = 15
-            };
-            waypoints.Add(newWp);
-            configIni.Set("waypoints", newWp.Name, $"{newWp.WorldPos.X:0.00},{newWp.WorldPos.Y:0.00},{newWp.WorldPos.Z:0.00},{newWp.Radius},{newWp.ApproachSpeed},");
-            Me.CustomData = configIni.ToString();
-        } else {
-            Frame(args);
-        }
-    } catch (Exception e) {
-        ScreenWriteln($"Error: {e}");
-    }
-
-    EndFrame();
-}
-
-private void Frame(string args) {
-
-    var currentPos = Me.GetPosition();
-
-    if (state == "init") {
-        if (waypoints.Count > 0) {
-            state = "followingWaypoints";
-            currentWaypointIndex = 0;
-        } else {
-            ScreenWriteln("No waypoints :(");
-        }
-    }
-
-    ScreenWriteln($"State = {state} ({frameCounter++})");
-
-
-    if (state == "waiting") {
-        if (args == "continue") {
-            state = "followingWaypoints";
-        }
-    }
-
-
-    if (state == "followingWaypoints") {
-        var idx = args != null && args != "" ? waypoints.FindIndex(w => w.Name == args) : -1;
-        if (idx != -1) {
-            currentWaypointIndex = idx;
-        } else {
-            var wp2 = waypoints[currentWaypointIndex];
-
-            if ((wp2.WorldPos - currentPos).Length() < wp2.Radius) {
-                currentWaypointIndex += 1;
-
-                foreach (var action in wp2.OnArriveActions) {
-                    if (action.Action == "wait") {
-                        state = "waiting";
-                        ResetThrusters();
-                    } else if (action.Action == "notify") {
-                        var block = (GridTerminalSystem.GetBlockWithName(action.ComputerName) as IMyProgrammableBlock);
-                        fuck = block == null ? "fuck" : "fuck???";
-                        fuck += action.ComputerName + "(" + action.Argument + ")";
-                        block?.TryRun(action.Argument);
-                    }
-                }
-
-            }
-
-            if (currentWaypointIndex >= waypoints.Count) {
-                currentWaypointIndex = 0;
-            }
-        }
-    }
-
-    if (state == "followingWaypoints") {
-
-        var wp = waypoints[currentWaypointIndex];
-
-        var worldToShipCoords = Matrix.Transpose(Me.WorldMatrix);
-
-        //ScreenWriteln($"At {currentPos.X:0.00} {currentPos.Y:0.00} {currentPos.Z:0.00}");
-        ScreenWriteln($"Heading to {wp.Name} {(wp.OnArriveActions.Count > 0 ? wp.OnArriveActions[0].Action : "none")}");
-        //ScreenWriteln($"Heading to {wp.WorldPos.X:0.00} {wp.WorldPos.Y:0.00} {wp.WorldPos.Z:0.00}");
-
-        var delta = Vector3D.TransformNormal(wp.WorldPos - currentPos, worldToShipCoords);
-        var currentVelocity = Vector3D.TransformNormal(controller.GetShipVelocities().LinearVelocity, worldToShipCoords);
-
-        //ScreenWriteln($"delta {delta.X:0.00} {delta.Y:0.00} {delta.Z:0.00}");
-        //ScreenWriteln($"currentVelocity {currentVelocity.X:0.00} {currentVelocity.Y:0.00} {currentVelocity.Z:0.00}");
-
-        var approachVelocity = Vector3D.Normalize(delta) * wp.ApproachSpeed;
-
-        var velocityDelta = approachVelocity - currentVelocity;
-
-        //ScreenWriteln($"approachVelocity {approachVelocity.X:0.00} {approachVelocity.Y:0.00} {approachVelocity.Z:0.00}");
-        //ScreenWriteln($"velocityDelta {velocityDelta.X:0.00} {velocityDelta.Y:0.00} {velocityDelta.Z:0.00}");
-        
-        if (Me.ShowOnHUD) {
-            ScreenWriteln("ShowOnHUD is set, autopilot not thrusting");
-            ResetThrusters();
-        } else {
-            FuckWithThrusters(velocityDelta.X, thrusters[Base6Directions.Direction.Left], thrusters[Base6Directions.Direction.Right], wp.ApproachSpeed);
-            FuckWithThrusters(velocityDelta.Y, thrusters[Base6Directions.Direction.Down], thrusters[Base6Directions.Direction.Up], wp.ApproachSpeed);
-            FuckWithThrusters(velocityDelta.Z, thrusters[Base6Directions.Direction.Forward], thrusters[Base6Directions.Direction.Backward], wp.ApproachSpeed);
-        }
-    }
-}
-
-private void ResetThrusters() {
-    foreach (var p in thrusters) {
-        foreach (var t in p.Value) {
-            t.ThrustOverridePercentage = 0;
-        }
-    }
-}
-
-private void FuckWithThrusters(double velocityDeltaComponent, List<IMyThrust> positiveThrusters, List<IMyThrust> negativeThrusters, double approachSpeed) {
-    var thrustSetting = (float) (Math.Abs(velocityDeltaComponent) / approachSpeed);
-    foreach (var t in positiveThrusters) {
-        t.ThrustOverridePercentage = velocityDeltaComponent > 0 ? thrustSetting : 0;
-    }
-    foreach (var t in negativeThrusters) {
-        t.ThrustOverridePercentage = velocityDeltaComponent < 0 ? thrustSetting : 0;
-    }
-}
-
-private string fuck = "";
-
-private void StartFrame() {
-    if (permanentlySavedInitString == null) {
-        permanentlySavedInitString = textOutputStringBuilder.ToString();
-    }
-    textOutputStringBuilder.Clear();
-}
-private void EndFrame() {
-    if (textOutputSurface != null) {
-        textOutputSurface.WriteText(permanentlySavedInitString);
-        textOutputSurface.WriteText(textOutputStringBuilder.ToString(), true);
-        textOutputSurface.WriteText(fuck, true);
-    } else {
-        Echo(permanentlySavedInitString);
-        Echo("textOutput not found :(\n" + textOutputStringBuilder.ToString());
-    }
-}
-
-private void ScreenWriteln(string s) {
-    textOutputStringBuilder.Append(s + "\n");
 }
 
 #region PreludeFooter
