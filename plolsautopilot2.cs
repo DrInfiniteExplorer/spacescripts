4651b751683f5152b466d5a8b02211a1e11bdaac
Pelle Månsson <pelle.mansson@gmail.com>
2021-02-05 22:01:31 +0100

    Update plolsautopilot2.cs

--- plolsautopilot2.cs
+++ plolsautopilot2.cs
@@ -16,6 +16,7 @@ using Sandbox.Game.EntityComponents;
 using SpaceEngineers.Game.ModAPI.Ingame;
 using VRage.Game.ObjectBuilders.Definitions;
 using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.GUI.TextPanel;
 
 // Change this namespace for each script you create.
 namespace SpaceEngineers.UWBlockPrograms.BatteryMonitor {
@@ -25,13 +26,17 @@ namespace SpaceEngineers.UWBlockPrograms.BatteryMonitor {
 
 private MyIni storageIni = new MyIni();
 private MyIni configIni = new MyIni();
-private IMyShipController controller;
 private string state;
 private List<Waypoint> waypoints;
-private ThrustController thrustController;
-private FrameText frameText;
+private ThrustController thrustController = new ThrustController();
+
+private FrameText frameText = new FrameText();
 private WaypointFollower waypointFollower;
 long frameCounter = 0;
+
+StableBlock<IMyShipController> controller;
+SometimesRecalculating<IMyTextSurface> textOutputSurface;
+SometimesRecalculating<Dictionary<Base6Directions.Direction, List<IMyThrust>>> thrusters;
 
 public Program() {
     Runtime.UpdateFrequency = UpdateFrequency.Update10;
@@ -39,30 +44,38 @@ public Program() {
     state = storageIni.Get("state", "state").ToString("init");
 
     var configParsedOk = configIni.TryParse(Me.CustomData);
-    var textOutputBlockName = configIni.Get("blocks", "textOutput").ToString().Split(':');
-    var textOutputBlock = GridTerminalSystem.GetBlockWithName(textOutputBlockName[0]);
-
-    frameText = new FrameText(textOutputBlock as IMyTextSurface ?? (textOutputBlock as IMyTextSurfaceProvider)
-        ?.GetSurface(textOutputBlockName.Length > 1 ? int.Parse(textOutputBlockName[1]) : 0));
     frameText.PreludeWriteln($"storage {storageParsedOk} config {configParsedOk}");
 
-    var controllerBlock = GridTerminalSystem.GetBlockWithName(configIni.Get("blocks", "controller").ToString());
-    controller = controllerBlock as IMyShipController;
+    var textOutputBlockName = configIni.Get("blocks", "textOutput").ToString().Split(':');
+
+    textOutputSurface = new SometimesRecalculating<IMyTextSurface>(g => {
+        var textOutputBlock = g.GetBlockWithName(textOutputBlockName[0]);
+        return (textOutputBlock as IMyTextSurface ?? (textOutputBlock as IMyTextSurfaceProvider)
+            ?.GetSurface(textOutputBlockName.Length > 1 ? textOutputBlockName[1].ParseInt() ?? 0 : 0));
+    });
+
+    controller = StableBlock<IMyShipController>.WithName(configIni.Get("blocks", "controller").ToString());
 
     var keys = new List<MyIniKey>();
     configIni.GetKeys("waypoints", keys);
     waypoints = keys.Select(key => Waypoint.FromString(key.Name, configIni.Get(key).ToString())).ToList();
 
     var allThrustersOnShip = new List<IMyThrust>();
-    GridTerminalSystem.GetBlocksOfType<IMyThrust>(allThrustersOnShip);
-    thrustController = new ThrustController(allThrustersOnShip);
+    var thrustersByDirection = new Dictionary<Base6Directions.Direction, List<IMyThrust>>();
+    thrusters = new SometimesRecalculating<Dictionary<Base6Directions.Direction, List<IMyThrust>>>(g => {
+        GridTerminalSystem.GetBlocksOfType<IMyThrust>(allThrustersOnShip);
+        ThrustController.DirectionalFromList(thrustersByDirection, allThrustersOnShip);
+        return thrustersByDirection;
+    });
 }
 public void Save() {
     storageIni.Set("state", "state", state);
     Storage = storageIni.ToString();
 }
 public void Main(string args) {
-    frameText.StartFrame();
+    frameText.StartFrame(textOutputSurface.Get(GridTerminalSystem));
+    thrustController.Attach(thrusters.Get(GridTerminalSystem));
+
     try {
         if (args == "createWaypoint") {
             var newWp = new Waypoint {
@@ -87,7 +100,7 @@ private void Frame(string args) {
     if (state == "init") {
         if (waypoints.Count > 0) {
             state = "followingWaypoints";
-            waypointFollower = new WaypointFollower(waypoints, controller, thrustController);
+            waypointFollower = new WaypointFollower(waypoints, controller.Get(GridTerminalSystem), thrustController);
         } else {
             frameText.Writeln("No waypoints :(");
         }
@@ -106,7 +119,7 @@ private void Frame(string args) {
     }
     if (state == "followingWaypoints") {
         frameText.Writeln($"Heading to {waypointFollower.Current.Name}");
-        waypointFollower.Go(Me.GetPosition(), Me.WorldMatrix, (s, wp, actions) => {
+                waypointFollower.Go(Me.GetPosition(), Me.WorldMatrix, (Action<string, Waypoint, List<OnArriveAction>>)((s, wp, actions) => {
             foreach (var action in actions) {
                 if (action.Action == "wait") {
                     state = "waiting";
@@ -117,9 +130,9 @@ private void Frame(string args) {
                 }
             }
             if (s == "finished") {
-                waypointFollower = new WaypointFollower(waypoints, controller, thrustController);
+                waypointFollower = new WaypointFollower(waypoints, controller.Get(GridTerminalSystem), thrustController);
             }
-        });
+        }));
     }
 }
 private class WaypointFollower {
@@ -150,18 +163,12 @@ private class WaypointFollower {
     }
 }
 private class ThrustController {
-    public Dictionary<Base6Directions.Direction, List<IMyThrust>> ThrustersByDirection { get; set; }
-    public ThrustController(List<IMyThrust> allowedThrusters) {
-        ThrustersByDirection = new Dictionary<Base6Directions.Direction, List<IMyThrust>>();
-        ThrustersByDirection[Base6Directions.Direction.Right] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(1, 0, 0)).ToList();
-        ThrustersByDirection[Base6Directions.Direction.Left] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(-1, 0, 0)).ToList();
-        ThrustersByDirection[Base6Directions.Direction.Up] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 1, 0)).ToList();
-        ThrustersByDirection[Base6Directions.Direction.Down] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(0, -1, 0)).ToList();
-        ThrustersByDirection[Base6Directions.Direction.Backward] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 0, 1)).ToList();
-        ThrustersByDirection[Base6Directions.Direction.Forward] = allowedThrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 0, -1)).ToList();
+    private Dictionary<Base6Directions.Direction, List<IMyThrust>> thrustersByDirection;
+    public void Attach(Dictionary<Base6Directions.Direction, List<IMyThrust>> allowedThrusters) {
+        thrustersByDirection = allowedThrusters;
     }
     public void ResetThrusters() {
-        foreach (var p in ThrustersByDirection) {
+        foreach (var p in thrustersByDirection) {
             foreach (var t in p.Value) {
                 t.ThrustOverridePercentage = 0;
             }
@@ -169,9 +176,9 @@ private class ThrustController {
     }
     public void ApproachVelocity(Vector3D currentVelocity, Vector3D desiredVelocity) {
         var velocityDelta = desiredVelocity - currentVelocity;
-        ControlThrusters(velocityDelta.X, ThrustersByDirection[Base6Directions.Direction.Left], ThrustersByDirection[Base6Directions.Direction.Right], desiredVelocity.Length());
-        ControlThrusters(velocityDelta.Y, ThrustersByDirection[Base6Directions.Direction.Down], ThrustersByDirection[Base6Directions.Direction.Up], desiredVelocity.Length());
-        ControlThrusters(velocityDelta.Z, ThrustersByDirection[Base6Directions.Direction.Forward], ThrustersByDirection[Base6Directions.Direction.Backward], desiredVelocity.Length());
+        ControlThrusters(velocityDelta.X, thrustersByDirection[Base6Directions.Direction.Left], thrustersByDirection[Base6Directions.Direction.Right], desiredVelocity.Length());
+        ControlThrusters(velocityDelta.Y, thrustersByDirection[Base6Directions.Direction.Down], thrustersByDirection[Base6Directions.Direction.Up], desiredVelocity.Length());
+        ControlThrusters(velocityDelta.Z, thrustersByDirection[Base6Directions.Direction.Forward], thrustersByDirection[Base6Directions.Direction.Backward], desiredVelocity.Length());
     }
     private void ControlThrusters(double velocityDeltaComponent, List<IMyThrust> positiveThrusters, List<IMyThrust> negativeThrusters, double approachSpeed) {
         var thrustSetting = (float) (Math.Abs(velocityDeltaComponent) / approachSpeed);
@@ -182,17 +189,26 @@ private class ThrustController {
             t.ThrustOverridePercentage = velocityDeltaComponent < 0 ? thrustSetting : 0;
         }
     }
+    public static void DirectionalFromList(Dictionary<Base6Directions.Direction, List<IMyThrust>> thrustersByDirection, IList<IMyThrust> thrusters) {
+        thrustersByDirection.Clear();
+        thrustersByDirection[Base6Directions.Direction.Right] = thrusters.Where(t => t.GridThrustDirection == new Vector3I(1, 0, 0)).ToList();
+        thrustersByDirection[Base6Directions.Direction.Left] = thrusters.Where(t => t.GridThrustDirection == new Vector3I(-1, 0, 0)).ToList();
+        thrustersByDirection[Base6Directions.Direction.Up] = thrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 1, 0)).ToList();
+        thrustersByDirection[Base6Directions.Direction.Down] = thrusters.Where(t => t.GridThrustDirection == new Vector3I(0, -1, 0)).ToList();
+        thrustersByDirection[Base6Directions.Direction.Backward] = thrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 0, 1)).ToList();
+        thrustersByDirection[Base6Directions.Direction.Forward] = thrusters.Where(t => t.GridThrustDirection == new Vector3I(0, 0, -1)).ToList();
+    }
 }
 private class FrameText {
     private StringBuilder preludeStringBuilder;
     private StringBuilder frameStringBuilder;
     private IMyTextSurface textOutputSurface;
-    public FrameText(IMyTextSurface surface) {
-        textOutputSurface = surface;
+    public FrameText() {
         preludeStringBuilder = new StringBuilder();
         frameStringBuilder = new StringBuilder();
     }
-    public void StartFrame() {
+    public void StartFrame(IMyTextSurface surface) {
+        textOutputSurface = surface;
         frameStringBuilder.Clear();
     }
     public void EndFrame() {
@@ -243,21 +259,56 @@ private struct Waypoint {
         var a = configLine.Split(',').ToList();
         return new Waypoint() {
             Name = name,
-            WorldPos = new Vector3D(DoubleFromString(a[0], 0), DoubleFromString(a[1], 0), DoubleFromString(a[2], 0)),
-            Radius = DoubleFromString(a[3], 0),
-            ApproachSpeed = DoubleFromString(a[4], 0),
+            WorldPos = new Vector3D(a[0].ParseDouble() ?? 0, a[1].ParseDouble() ?? 0, a[2].ParseDouble() ?? 0),
+            Radius = a[3].ParseDouble() ?? 0,
+            ApproachSpeed = a[4].ParseDouble() ?? 0,
             OnArriveActions = OnArriveAction.FromString(a[5]),
         };
     }
-    private static double DoubleFromString(string s, double defaultValue) {
-        double d;
-        if (double.TryParse(s.Trim(), out d)) {
-            return d;
-        } else {
-            return defaultValue;
-        }
+}
+
+private struct StableBlock<T> where T : class, IMyTerminalBlock {
+    private Func<IMyGridTerminalSystem, T> lookup;
+    private T block;
+    public T Get(IMyGridTerminalSystem g) {
+        if (block == null || g.GetBlockWithId(block.EntityId) == null) { block = lookup(g); }
+        return block;
+    }
+    private StableBlock(Func<IMyGridTerminalSystem, T> slowLookup) {
+        lookup = slowLookup;
+        block = default(T);
+    }
+    public static StableBlock<T> WithName(string name) {
+        return new StableBlock<T>((g) => g.GetBlockWithName(name) as T);
+    }
+    public static StableBlock<T> Custom(Func<IMyGridTerminalSystem, T> f) {
+        return new StableBlock<T>(f);
     }
 }
+
+private struct SometimesRecalculating<T> where T : class {
+    private static Random random = new Random();
+    private T cache;
+    private Func<IMyGridTerminalSystem, T> lookup;
+    public T Get(IMyGridTerminalSystem gridTerminalSystem) {
+        if (cache == null || random.NextDouble() < 0.01) { cache = lookup(gridTerminalSystem); }
+        return cache;
+    }
+    public SometimesRecalculating(Func<IMyGridTerminalSystem, T> slowLookup) {
+        lookup = slowLookup;
+        cache = null;
+    }
+}
+
+} public static class Utils {
+    public static double? ParseDouble(this string s) {
+        double x;
+        return double.TryParse(s, out x) ? (double?)x : null;
+    }
+    public static int? ParseInt(this string s) {
+        int x;
+        return int.TryParse(s, out x) ? (int?)x : null;
+    }
 
 #region PreludeFooter
     }
